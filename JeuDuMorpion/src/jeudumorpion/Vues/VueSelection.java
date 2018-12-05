8f1ded639654b4735886063b4101f6825c1f6417
mirasl <mirasl@pc-dg-037-12.iut2.upmf-grenoble.fr>
2018-12-05 11:29:23 +0100

    Création Vue Selection

---
+++ JeuDuMorpion/src/jeudumorpion/Vues/VueSelection.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeudumorpion.Vues;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author mirasl
+ */
+public class VueSelection {
+
+    private final JFrame window;
+    private final JButton btnNewPartie;
+    private final JButton btnTableauS;
+
+    public VueSelection(){
+        window = new JFrame();
+        window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        // Définit la taille de la fenêtre en pixels
+        window.setSize(600, 600);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation(dim.width / 2 - window.getSize().width / 2, dim.height / 2 - window.getSize().height / 2);
+        
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        window.add(mainPanel);
+        
+        //Positionnement titre
+        mainPanel.add(new JLabel(), BorderLayout.NORTH);
+        JLabel labelTitre = new JLabel("Jeu du morpion") ;
+        labelTitre.setForeground(Color.BLACK);
+        labelTitre.setFont(new Font(labelTitre.getFont().getName(), labelTitre.getFont().getStyle(), (int) (labelTitre.getFont().getSize() * 1.5)));
+        mainPanel.add(labelTitre, BorderLayout.NORTH);
+        mainPanel.add(new JLabel(), BorderLayout.NORTH);
+        
+        //création zone menu
+        JPanel menu= new JPanel(new BorderLayout());
+        mainPanel.add(menu,BorderLayout.CENTER);
+        //menuHaut parti
+        JPanel menuHaut = new JPanel(new GridLayout(5,3));
+        menu.add(menuHaut,BorderLayout.NORTH);
+        btnNewPartie = new JButton("Nouvelle Partie");
+        btnTableauS = new JButton("Tableau des Scores");
+        for(int x =0; x<15;x++){
+            if (x==2){
+                menuHaut.add(btnNewPartie);
+            }
+            else if(x==8){
+                menuHaut.add(btnTableauS);                
+            }
+            else {
+                menuHaut.add(new JLabel());
+            }
+        }
+        
+    }
+}
